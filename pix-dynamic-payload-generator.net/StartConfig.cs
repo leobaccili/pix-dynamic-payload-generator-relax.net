3965f9e797da4774f1a1c1861cbe233661a9fc23
Léo Baccili <leobaccili@bacctecnologia.com.br>
2023-07-13 07:40:19 -0300

    better summary

--- pix-dynamic-payload-generator.net/StartConfig.cs
+++ pix-dynamic-payload-generator.net/StartConfig.cs
@@ -22,14 +22,43 @@ namespace pix_dynamic_payload_generator.net
 
         public static X509Certificate2 Certificate { get; private set; }
 
+        /// <summary>
+        /// Endpoint para autenticação
+        /// </summary>
         public static string AuthEndpoint { get; private set; }
 
+        /// <summary>
+        /// Utilizar UrlFormEncoded para autenticação ao invés de json
+        /// </summary>
         public static bool UseUrlFormEncodedForAuth { get; private set; }
 
+        /// <summary>
+        /// Scopos de autenticação
+        /// </summary>
         public static string AuthScopes { get; private set; }
 
+        /// <summary>
+        /// Usar área antes dos endpoints. 
+        /// Na implantação padrão as urls do pix por exemplo são urlbase/v2/cobv
+        /// Já no banco inter, as urls são urlbase/pix/v2/cobv
+        /// </summary>
         public static bool UseEndpointArea { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_baseUrl">Url da api pix do PSP, exemplo: https://api-pix-h.gerencianet.com.br</param>
+        /// <param name="_clientId">ClientId do oauth 2 do PSP</param>
+        /// <param name="_clientSecret">ClientSecret do oauth 2 do PSP</param>
+        /// <param name="_certificate">Certificado do PSP</param>
+        /// <param name="_authEnpoint">Endpoint para autenticação</param>
+        /// <param name="_useUrlFormEncodedForAuth">Utilizar UrlFormEncoded para autenticação ao invés de json</param>
+        /// <param name="_authScopes">Escopos de autenticação</param>
+        /// <param name="_useEndpointArea">
+        /// Usar área antes dos endpoints. 
+        /// Na implantação padrão as urls do pix por exemplo são urlbase/v2/cobv
+        /// Já no banco inter, as urls são urlbase/pix/v2/cobv
+        /// </param>
         public StartConfig(
             string _baseUrl,
             string _clientId,
